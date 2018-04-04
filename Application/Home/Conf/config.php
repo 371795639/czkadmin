1394b920579157c239cf18eab9e4bf0a6d73b246
administrator <administrator@administratordeMacBook-Air.local>
2018-04-04 17:32:30 +0800

    second commit

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -3,9 +3,9 @@
 class AuthorConfig {
 
     // 图像识别授权
-    const IMG_APP_ID = '11036224';
-    const IMG_API_KEY = 'oYYANgMFCHPHl3wlrkocEHsb';
-    const IMG_SECRET_KEY = 'P0yLc6tyryUTr1KIFOTRcdxG7NDiOGDR';
+    const IMG_APP_ID = '';
+    const IMG_API_KEY = '';
+    const IMG_SECRET_KEY = '';
 
     //
 }
